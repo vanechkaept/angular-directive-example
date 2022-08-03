a7479ef83d07743c40ec481916daee3984b312ec
vanechkaept <vanekkgj@gmail.com>
2022-08-03 16:55:55 +0300

    structural directive

--- src/app/custom.directive.ts
+++ src/app/custom.directive.ts
@@ -5,63 +5,25 @@ import {
   HostListener,
   Input,
   Renderer2,
+  TemplateRef,
+  ViewContainerRef,
 } from '@angular/core';
 
 @Directive({
   selector: '[dir]',
-  host: {
-    '(mouseover)': 'onHover($event)',
-  },
 })
 export class CustomDirective {
-  @Input() textSize = '20px';
-  @Input() defaultTextSize = '1rem';
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-    console.log('sdsdad');
-    // this.elementRef.nativeElement.style.fontWeight = 'bold';
-    setTimeout(() => this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'fontSize',
-      this.defaultTextSize
-    ));
-  }
-
-  @HostBinding('style.cursor') get getCursor() {
-    return 'pointer';
-  }
-
-  @HostBinding('style.color') get getColor() {
-    return 'red';
-  }
-
-  onHover(event: KeyboardEvent) {
-    console.log(event);
-    this.changeFontWeight('bold');
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'green');
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'fontSize',
-      this.textSize
-    );
-  }
-
-  @HostListener('mouseout', ['$event'])
-  onOut(event: KeyboardEvent) {
-    console.log(event);
-    this.changeFontWeight('normal');
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'fontSize',
-      this.defaultTextSize
-    );
-  }
-
-  private changeFontWeight(newValue: string) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'fontWeight',
-      newValue
-    );
+  @Input() set dir(condition: boolean) {
+    if (condition) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
   }
 }
