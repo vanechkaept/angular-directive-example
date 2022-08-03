1a07a2c88824050189933de81ff3a1d699afef61
vanechkaept <vanekkgj@gmail.com>
2022-08-03 16:19:01 +0300

    2 files modified

--- src/app/custom.directive.ts
+++ src/app/custom.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   Renderer2,
 } from '@angular/core';
 
@@ -13,8 +14,17 @@ import {
   },
 })
 export class CustomDirective {
+  @Input() textSize = '20px';
+  @Input() defaultTextSize = '1rem';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    console.log('sdsdad');
     // this.elementRef.nativeElement.style.fontWeight = 'bold';
+    setTimeout(() => this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'fontSize',
+      this.defaultTextSize
+    ));
   }
 
   @HostBinding('style.cursor') get getCursor() {
@@ -29,12 +39,22 @@ export class CustomDirective {
     console.log(event);
     this.changeFontWeight('bold');
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'green');
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'fontSize',
+      this.textSize
+    );
   }
 
   @HostListener('mouseout', ['$event'])
   onOut(event: KeyboardEvent) {
     console.log(event);
     this.changeFontWeight('normal');
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'fontSize',
+      this.defaultTextSize
+    );
   }
 
   private changeFontWeight(newValue: string) {
